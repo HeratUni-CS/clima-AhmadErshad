4130eff8980403a8e0b3271ae9f2eaf8aab6fbbf
Ahmad Ershad Omari <ahmad.ershad.2014@gmail.com>
2023-06-12 15:32:27 +0430

    geolocator package and permission of place

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  void getLocation() async {
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    // ignore: avoid_print
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,8 +23,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: TextButton(
           onPressed: () {
             //Get the current location
+            getLocation();
           },
-          child: Text('Get Location'),
+          child: const Text('Get Location'),
         ),
       ),
     );
